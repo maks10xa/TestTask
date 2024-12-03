db162e5df4a3074a396a424060c4d2482476dc49
Максим <yanchevskiy.mv@intechs.by>
2024-12-03 18:02:43 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService(ApplicationDbContext context) : IAuthorService
+    {
+        private readonly ApplicationDbContext _context = context;
+        private readonly int _selectedYear = 2015;
+
+        public async Task<Author> GetAuthor()
+        {
+            var authorsWithLongestTitles = await _context.Authors.Select(a => new
+            {
+                Author = a,
+                MaxTitleLength = _context.Books.Where(b => b.AuthorId == a.Id).Max(b => b.Title.Length)
+            }).ToListAsync();
+
+            var author = authorsWithLongestTitles
+                            .OrderByDescending(i => i.MaxTitleLength)
+                            .ThenBy(i => i.Author.Id)
+                            .FirstOrDefault().Author;
+            return author;
+        }
+
+        public async Task<List<Author>> GetAuthors() => await _context.Books
+        .Where(b => b.PublishDate.Year > _selectedYear)
+        .GroupBy(b => b.AuthorId)
+        .Where(i => i.Count() % 2 == 0)
+        .Select(i => _context.Authors.FirstOrDefault(a => a.Id == i.Key)) 
+        .AsNoTracking()
+        .ToListAsync();
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService(ApplicationDbContext context) : IBookService
+    {
+        private readonly ApplicationDbContext _context = context;
+        private readonly string _requiredTitleWord = "Red";
+        private readonly DateTime _carolusRexReleaseDate = new DateTime(2012, 5, 22, 12, 0, 0);
+
+        public async Task<Book> GetBook() => await _context.Books
+            .OrderByDescending(i => i.Price * i.QuantityPublished)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        public async Task<List<Book>> GetBooks() => await _context.Books
+            .Where(b => b.Title.Contains(_requiredTitleWord) && b.PublishDate > _carolusRexReleaseDate)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
